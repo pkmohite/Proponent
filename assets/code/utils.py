1ee697fd4be5dfffa73c4476294bae3ad22033bc
pkmohite <pmohite95@gmail.com>
2024-06-16 18:11:31 -0700

    Refactor PDF display in Sales Enablement page

--- assets/code/utils.py
+++ assets/code/utils.py
@@ -344,10 +344,10 @@ def displayPDF(file, column = st, width = 840, height = 520):
         base64_pdf = base64.b64encode(f.read()).decode("utf-8")
 
     # Embedding PDF in HTML
-    # pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="1000" height="600" type="application/pdf">'
+    pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="1000" height="600" type="application/pdf">'
     
     # Method 2 - Using IFrame
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width={width} height={height} type="application/pdf"></iframe>'
+    # pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width={width} height={height} type="application/pdf"></iframe>'
 
     # Displaying File
     column.markdown(pdf_display, unsafe_allow_html=True)
